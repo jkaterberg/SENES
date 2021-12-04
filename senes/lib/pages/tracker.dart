a9d986d5019aaa81ea9cc84eec5262580ba14da2
Justin Katerberg <j.katerberg10@gmail.com>
2021-12-03 19:29:12 -0500

    fix imports

--- senes/lib/pages/tracker.dart
+++ senes/lib/pages/tracker.dart
@@ -4,7 +4,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:latlong2/latlong.dart';
-import 'package:senes/helpers/env_conditions.dart';
 import 'package:senes/helpers/route_point.dart';
 import 'package:senes/widgets/map.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
